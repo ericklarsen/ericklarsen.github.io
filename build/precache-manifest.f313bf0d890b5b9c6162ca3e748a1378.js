cb6f3af89937658f0b3583a049b693cad0110f83
ericklarsen <ericklarsenn@gmail.com>
2020-09-07 11:53:07 +0700

    Update Readme

---
+++ build/precache-manifest.f313bf0d890b5b9c6162ca3e748a1378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e8eb7279f14cd3ede88d002245209b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "132f89aeb9bdd21448dd",
+    "url": "/static/css/main.7169de39.chunk.css"
+  },
+  {
+    "revision": "4bd1821cb42cf4c75ec7",
+    "url": "/static/js/2.3c460198.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c460198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132f89aeb9bdd21448dd",
+    "url": "/static/js/main.7f20e77d.chunk.js"
+  },
+  {
+    "revision": "a7fc65e03dec489b804a",
+    "url": "/static/js/runtime-main.3d8a6125.js"
+  }
+]);
